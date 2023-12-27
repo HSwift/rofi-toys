15a49b32b7e528e1694e794ee98da99a803ff7f8
HSwift <18740745+HSwift@users.noreply.github.com>
2023-12-27 23:32:00 +0800

    feat: add clipd config items

--- src/clipd/main.rs
+++ src/clipd/main.rs
@@ -1,11 +1,29 @@
+mod http_rpc;
 mod manager;
 mod stroage;
-mod http_rpc;
 
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow};
 use manager::ClipboardManager;
-use rofi_toys::clipboard;
+use rofi_toys::{clipboard, file};
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct ClipdConfig {
+    max_size: usize,
+}
+
+fn generate_default_config() -> ClipdConfig {
+    let clipd_config = ClipdConfig { max_size: 32 };
+    file::config_save_to_file(&clipd_config, "clipd").unwrap();
+    return clipd_config;
+}
+
+fn read_config() -> ClipdConfig {
+    match file::config_restore_from_file("clipd") {
+        Ok(x) => x,
+        Err(_) => generate_default_config(),
+    }
+}
 
 fn main() {
     let app = Application::builder()
@@ -13,8 +31,10 @@ fn main() {
         .build();
 
     app.connect_activate(|app| {
+        let clipd_config = read_config();
         let clipboard = gtk::Clipboard::get(&gtk::gdk::SELECTION_CLIPBOARD);
-        let manager = ClipboardManager::new(32, clipboard::get_clipd_listen_path());
+        let manager =
+            ClipboardManager::new(clipd_config.max_size, clipboard::get_clipd_listen_path());
 
         clipboard.connect("owner-change", false, move |values| {
             let clipboard = values[0]
